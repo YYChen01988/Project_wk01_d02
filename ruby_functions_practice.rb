e0ab43518eb15b64587e14c0c60cb86a496065dc
yingying <chen.yingying.01988@gmail.com>
2018-07-17 15:51:27 +0100

    add function 01-08

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -9,3 +9,23 @@ end
 def subtract(a,b)
   return a-b
 end
+
+def multiply(a, b)
+  return a*b
+end
+
+def divide(a,b)
+  return a/b
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(s1, s2)
+  return s1 << s2
+end
+
+def add_string_as_number(s1, s2)
+  return s1.to_i + s2.to_i
+end
